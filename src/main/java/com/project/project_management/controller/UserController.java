3eaeaca5e2e677c42de456e30c5c4f6db7a964ae
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2025-01-24 14:06:23 +0100

    implemented field level validation and handled exceptions

--- src/main/java/com/project/project_management/controller/UserController.java
+++ src/main/java/com/project/project_management/controller/UserController.java
@@ -3,6 +3,7 @@ package com.project.project_management.controller;
 import com.project.project_management.dto.PropertyDTO;
 import com.project.project_management.dto.UserDTO;
 import com.project.project_management.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,16 @@ public class UserController {
     @Autowired
     public UserService userService;
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserDTO userDTO){
         userDTO=userService.registerUser(userDTO);
         ResponseEntity<UserDTO> responseEntity = new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+        return responseEntity;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@RequestBody UserDTO userDTO){
+        userDTO=userService.loginUser(userDTO.getOwnerEmail(),userDTO.getPassword());
+        ResponseEntity<UserDTO> responseEntity = new ResponseEntity<>(userDTO, HttpStatus.OK);
         return responseEntity;
     }
 }
--- src/main/java/com/project/project_management/dto/UserDTO.java
+++ src/main/java/com/project/project_management/dto/UserDTO.java
@@ -5,13 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
     private Long Id;
     private String ownerName;
+    @NotNull(message = "Email is mandatory")
+    @Size(min=1 , max=50 , message = "Email should be between 1 to 50 characters in length")
     private String ownerEmail;
+    @NotNull(message = "Password cannot be null")
+    @Size(min=8,max=20,message = "Password cannot shorter")
     private String password;
     private String phone;
 
